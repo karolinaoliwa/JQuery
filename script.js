96805b67c3b4fdd84a3ef427240d754b867b9004
Karolina <karolina@Air-Karolina.lan>
2018-01-10 12:46:05 +0100

    Add files.

---
+++ script.js
@@ -0,0 +1,10 @@
+function getTriangleArea(a, h) {
+    if (a <= 0 || h <= 0 ) {
+        alert('Invalid value.');
+      }  return(a*h/2);
+    }
+    console.log(getTriangleArea(10, 6));    
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(20, 30);
+var triangle3Area = getTriangleArea(30, 45);
