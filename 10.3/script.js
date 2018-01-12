9af2f72d8a025c1eae45d22814a4aba7cc10b92d
Karolina <karolina@Air-Karolina.lan>
2018-01-12 13:36:56 +0100

    Add changes.

---
+++ 10.3/script.js
@@ -0,0 +1,12 @@
+$(function(){
+	var carouselList = $("#carousel ul");
+	setInterval(changeSlide, 3000);
+	function changeSlide() {
+        carouselList.animate({'marginLeft':-400}, 500, 
+        function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem); }
+        carouselList.css({marginLeft:0});
+    }
+}
