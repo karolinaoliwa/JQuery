4d7fb35bf008ef188696fd77700a4e1ed5f7c36d
Karolina <karolina@Air-Karolina.lan>
2018-01-15 17:59:52 +0100

    Add changes to carousel.

--- 10.3/script.js
+++ 10.3/script.js
@@ -1,4 +1,4 @@
-$(function(){
+/*$(function(){
 	var carouselList = $("#carousel ul");
 	setInterval(changeSlide, 3000);
 	function changeSlide() {
@@ -9,4 +9,19 @@ $(function(){
 		lastItem.after(firstItem); }
         carouselList.css({marginLeft:0});
     }
-}
+}); */
+
+$(function(){
+	var carouselList = $("#carousel ul");
+    setInterval(changeSlide, 3000);
+    function changeSlide(){
+        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);    
+    }
+    function moveFirstSlide(){
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    }
+});
